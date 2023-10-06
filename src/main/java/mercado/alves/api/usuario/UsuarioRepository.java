df3e0138873d7cbb2958adc5b51bb7d2308a4d08
ricvaz2008 <111580288+ricvaz2008@users.noreply.github.com>
2023-10-06 07:17:11 -0400

    Tirada ultima referencia ao flyway

--- src/main/java/mercado/alves/api/usuario/UsuarioRepository.java
+++ src/main/java/mercado/alves/api/usuario/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package mercado.alves.api.usuario;
 
-import org.flywaydb.core.internal.util.JsonUtils;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
