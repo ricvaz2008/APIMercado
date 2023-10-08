1f70c9f141dcfbfc2f0a7685729871b89c481d37
ricvaz2008 <111580288+ricvaz2008@users.noreply.github.com>
2023-10-08 00:17:41 -0400

    Tela de funcionarios concluida

--- src/main/java/mercado/alves/api/controller/UsuarioController.java
+++ src/main/java/mercado/alves/api/controller/UsuarioController.java
@@ -1,14 +1,20 @@
 package mercado.alves.api.controller;
 
 import jakarta.validation.Valid;
+import mercado.alves.api.cliente.Cliente;
+import mercado.alves.api.cliente.DadosCadastroCliente;
 import mercado.alves.api.usuario.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("usuarios")
@@ -20,8 +26,11 @@ public class UsuarioController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroUsuario dados) {
+    public ResponseEntity<Map<String, String>> cadastrar(@RequestBody @Valid DadosCadastroUsuario dados) {
         repository.save(new Usuario(dados));
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "itemCadastrado");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping
@@ -32,16 +41,26 @@ public class UsuarioController {
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosCadastroUsuario dados) {
-        var funcionario = repository.getReferenceById(dados.id());
-        funcionario.atualizarInformacoes(dados);
+    public ResponseEntity<String> atualizar(@RequestBody @Valid DadosCadastroUsuario dados) {
+        try {
+            var usuario = repository.getReferenceById(dados.id());
+            usuario.atualizarInformacoes(dados);
 
+            return ResponseEntity.ok("{\"message\": \"modificado\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"An error occurred\"}");
+        }
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
-        repository.deleteById(id);
+    public ResponseEntity<String> excluir(@PathVariable Long id) {
+        try {
+            repository.deleteById(id);
+            return ResponseEntity.ok("{\"message\": \"itemApagado\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"message\": \"Erro ao apagar o item\"}");
+        }
     }
 
     @GetMapping("/verifica-acesso")
